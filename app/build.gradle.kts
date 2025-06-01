5ba73c6a776fbdb61e39f5def6de1bebca38ab80
De General <aubynsamuel05@gmail.com>
2025-06-01 11:50:37 +0000

    updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,4 +95,7 @@ dependencies {
     implementation(libs.hilt.android)
     implementation(libs.androidx.hilt.navigation.compose)
     ksp(libs.hilt.android.compiler)
+
+    implementation(libs.material3)
+
 }
