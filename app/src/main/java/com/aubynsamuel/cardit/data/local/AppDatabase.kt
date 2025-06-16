3f46dc8aebbba035bd46019505d3eb2e90388e37
De General <aubynsamuel05@gmail.com>
2025-06-16 04:30:49 +0000

    Dependencies update

--- app/src/main/java/com/aubynsamuel/cardit/data/local/AppDatabase.kt
+++ app/src/main/java/com/aubynsamuel/cardit/data/local/AppDatabase.kt
@@ -20,7 +20,7 @@ abstract class AppDatabase : RoomDatabase() {
                     AppDatabase::class.java,
                     "contact_scanner_db"
                 )
-                    .fallbackToDestructiveMigration()
+                    .fallbackToDestructiveMigration(false)
                     .build()
                 INSTANCE = instance
                 instance
